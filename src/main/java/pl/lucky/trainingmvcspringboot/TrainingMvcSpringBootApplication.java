6e36177dd79aaaae8c6285f2e6c67a0b7f3c8578
lucki90 <tomlucki@wp.pl>
2019-06-25 14:26:14 +0200

    init

---
+++ src/main/java/pl/lucky/trainingmvcspringboot/TrainingMvcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package pl.lucky.trainingmvcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingMvcSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingMvcSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/lucky/trainingmvcspringboot/TrainingMvcSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.lucky.trainingmvcspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingMvcSpringBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
